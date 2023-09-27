52e03be02275f52b9c28e9881d0eb74346c0104b
jerin-arpa <ismatjerrin3@gmail.com>
2023-09-27 16:08:08 +0600

    create aboutus, contactus and login page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,32 @@
 const Contact = () => {
     return (
         <div>
+            <div className="hero" style={{ backgroundImage: 'url(/public/background.jpg)', backgroundAttachment: 'fixed' }}>
+                <div className="hero-overlay bg-black bg-opacity-75"></div>
+                <div className="hero-content text-center container mx-auto px-5">
+                    <div className="max-w-2xl py-32">
+                        <h1 className="mb-8 text-5xl font-bold"><span className="text-white">Contact at <br /></span> <span className="text-amber-600">All Instrument Academy</span></h1>
+
+
+                        <div className="flex justify-center container mx-auto px-5 my-14">
+                            <div>
+                                <div className="mt-5">
+                                    <input type="text" placeholder="Name" className="input input-bordered border-amber-600  w-full mb-5" />
+                                    <input type="email" placeholder="Email Address" className="input input-bordered border-amber-600  w-full mb-5" />
+                                    <input type="number" placeholder="Phone Number" className="input input-bordered border-amber-600  w-full mb-5" />
+                                    <textarea placeholder="Comment" className="textarea textarea-bordered textarea-lg w-full p-0 pl-3 border-amber-600" ></textarea>
+
+                                    <div className="flex justify-center mt-10">
+                                        <button className="btn bg-amber-600 border-amber-600 text-white rounded-md text-xl w-full">Submit</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
 
         </div>
     );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,19 @@ const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            <h2>All classes</h2>
+
+            <div className="my-16">
+                <h2 className="text-xl text-amber-600 font-bold text-center mb-2">Our Classes</h2>
+                <h1 className="text-4xl text-cyan-900 font-bold text-center mb-4">Most Popular Classes</h1>
+                <div className="flex justify-center">
+                    <p className="w-2/3 text-cyan-900 text-center mb-10">Pellentesque mattis mauris ac tortor volutpat, eu fermentum sapien euismod. <br /> In id tempus metus. Donec eu volutpat nibh, in maximus ligula.</p>
+                </div>
+            </div>
+
+
+            <div>
+
+            </div>
         </div>
     );
 };
