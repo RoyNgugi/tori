7e09522cef72dd5984bc0dbcba354e4b73d09a43
RoyNgugi <royngugi69@gmail.com>
2025-04-08 20:48:20 +0300

    Final

--- src/components/Login/firebase.js
+++ src/components/Login/firebase.js
@@ -8,13 +8,13 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD8EfmgbBSTH4x_A9dFOdO4qQVm8Db54Ac",
-  authDomain: "tori-606a9.firebaseapp.com",
-  projectId: "tori-606a9",
-  storageBucket: "tori-606a9.firebasestorage.app",
-  messagingSenderId: "14422679953",
-  appId: "1:14422679953:web:e1cc50d2974f240e6dfa30",
-  measurementId: "G-LPKH2YZ13Q"
+  apiKey: ,
+  authDomain: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: ,
+  measurementId: 
 };
 
 // Initialize Firebase
